5828c2a3b4a18b4886a7aaf4edc18357683b89fd
richard12390 <richard12390s@gmail.com>
2024-03-27 15:17:39 +0800

    update files

---
+++ borneoexotics_webcrawler.py
@@ -0,0 +1,103 @@
+import requests
+import time
+import os
+from image_data_multilabel_prediction_function import image_data_multilabel_prediction
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from rows_dict_to_csv_function import rows_dict_to_csv
+
+opts = Options()
+opts.add_argument("--window-size=240,1440")
+
+
+URL = "https://borneoexotics.net/shop/"
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opts)
+driver.get(URL)
+
+species_group = ['truncata','veitchii','ventricosa']
+folder_path = 'imgs'
+
+def store_image_and_csv(folder_path, species_group):
+
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    
+    search_images_ul = driver.find_element(By.XPATH,'//*[@id="shop-isle-blog-container"]/ul[1]')
+    search_images_li = search_images_ul.find_elements(By.TAG_NAME, 'li')
+    print("len(search_images_li):",len(search_images_li))
+
+    for images_class in search_images_li:
+        ext = images_class.find_element(By.TAG_NAME, 'h2').text
+        print(ext)
+        if '–' not in ext:
+            species = ext.split(':')[0].strip()  
+            serial = ext.split('.')[-1].split(' ')[-1]              
+            source = "unknown"    
+        else:                  
+            species = ext.split('–')[0].strip()                
+            serial = ext.split('.')[-1].split(' ')[-1]
+            source = ext.split('–')[-1].split(':')[0].strip()          
+            
+        images = images_class.find_elements(By.TAG_NAME, 'img')[0]
+        IMAGE_URL = images.get_attribute('src')            
+        headers = {'User-Agent': 'PostmanRuntime/7.29.0'}
+        img_resource = requests.get(IMAGE_URL, headers=headers)
+        if img_resource.status_code == 200:
+            file_path = open(os.path.join(folder_path,f'{species}_{serial}_{source}.jpg'), 'wb')
+            file_path.write(img_resource.content)
+            file_path.close()
+            print("file_path",file_path)
+        else:
+            print('status code error')  
+
+    rows_dict = image_data_multilabel_prediction(folder_path, species_group=species_group, model_structure="model_structure.json", model_weights="model_weights.h5")
+
+    rows_dict_to_csv(rows_dict,species_group=species_group, csvfile="images.csv")
+
+    img_resource.close()
+ 
+def img_webcrawler(folder_path, species_group):
+    locator = (By.ID,"popmake-368")
+    try:
+        WebDriverWait(driver, 60).until(ec.visibility_of_element_located(locator))      
+        driver.find_element(By.XPATH,'//*[@id="popmake-368"]/button').click()    
+        time.sleep(2)
+    except:
+        pass
+
+    for species in species_group:
+        print("enter species" )
+        search_species = driver.find_element(By.XPATH,'//*[@id="woocommerce-product-search-field-0"]')
+        print("search_species:",search_species)
+        search_species.clear()
+        search_species.send_keys(species)
+        search_species.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+        
+        try:
+            page_div = driver.find_element(By.XPATH,'//*[@id="shop-isle-blog-container"]/div/div/nav')
+            page_elements = page_div.find_elements(By.TAG_NAME, 'li')   
+            for page in range(len(page_elements)-1):
+                
+                try:        
+                    next_page = driver.find_element(By.CLASS_NAME, 'next') 
+                    print("next_page:",next_page.text)
+                    
+                    next_page.click()                
+                    store_image_and_csv(folder_path, species_group)
+                except:
+                    print("except")
+                    pass
+        except:
+            pass  
+
+    driver.quit()
+
+img_webcrawler(folder_path, species_group)
---
+++ nepenthes_multilabel_classification.py
@@ -0,0 +1,155 @@
+
+#%%
+import os
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+
+def fix_gpu():
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+fix_gpu()
+
+# preprocessing images 
+train_dir = os.path.join("input", "train")
+test_dir = os.path.join("input", "test")
+
+# image preprocessing 
+batch_size = 16
+epochs = 100
+
+# image augmentation
+train_data = ImageDataGenerator(
+     rescale=1.0 / 255,
+     horizontal_flip=True,
+     zoom_range=0.1,
+     width_shift_range=0.1,
+     height_shift_range=0.1,
+     rotation_range=30,
+     vertical_flip=False,
+)
+train_images = train_data.flow_from_directory(
+     directory=train_dir,
+     target_size = (128,128),
+     class_mode="binary", batch_size=batch_size, shuffle=True, subset='training' #training split from training data
+)
+
+labels = (train_images.class_indices)
+labels = dict((value,key) for key,value in labels.items())
+
+
+
+# model build
+from sklearn.model_selection import KFold 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, Dropout, MaxPooling2D,  BatchNormalization,Activation
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint 
+
+model = Sequential()
+
+model.add(Conv2D(filters = 64,
+                    kernel_size = (7,7),
+                    padding='same',
+                    activation = "tanh",
+                    input_shape = (128,128,3)))
+model.add(BatchNormalization())
+model.add(Conv2D(filters = 64,
+                    kernel_size = (7,7),
+                    padding='same',
+                    activation = "relu",
+                    input_shape = (128,128,3)))
+
+model.add(BatchNormalization())  
+model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Dropout(0.1))
+
+model.add(Conv2D(filters = 64,
+                    kernel_size = (7,7),
+                    padding='same',
+                    activation = "relu",
+                    input_shape = (128,128,3)))
+
+model.add(BatchNormalization())
+model.add(Conv2D(filters = 64,
+                    kernel_size = (3,3),
+                    padding='same',
+                    activation = "relu",
+                    input_shape = (128,128,3)))
+
+model.add(BatchNormalization())  
+model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Dropout(0.4)) 
+     
+model.add(Flatten())
+
+model.add(Dense(700,
+               activation = "tanh"))
+model.add(BatchNormalization()) 
+model.add(Dropout(0.25)) 
+model.add(Dense(700,
+               activation = "tanh"))
+model.add(BatchNormalization()) 
+model.add(Dropout(0.4))    
+
+model.add(Dense(units = 3, activation = 'sigmoid'))
+
+model.compile(
+    optimizer= 'rmsprop', 
+    loss="binary_crossentropy",
+    metrics=['accuracy', 'f1_score', 'average_precision'])
+
+earlyStopping = EarlyStopping(  patience=2,
+                                monitor='val_f1_score',
+                                restore_best_weights=True)
+
+modelcheckpoint = ModelCheckpoint( 'model_weights.h5',
+                                    monitor = 'val_auc',
+                                    verbosesave_best_only = True)
+
+kf = KFold(n_splits=5, shuffle=True, random_state=42)
+fold_results = []
+
+for fold, train_index, val_index in enumerate(kf.split(train_images), 1):
+    X_train, X_val = train_images[train_index], train_images[val_index]
+    y_train, y_val = labels[train_index], labels[val_index]
+
+    # train model
+    result = model.fit(
+        X_train,
+        validation_data=X_val,
+        batch_size=batch_size,
+        epochs=epochs,
+        callbacks=[earlyStopping, modelcheckpoint],
+        verbose=1)
+    fold_results.append(result.history)
+
+for fold, result in enumerate(fold_results, 1):
+    print(f"Fold {fold} - Accuracy: {result['accuracy'][-1]}, Loss: {result['loss'][-1]}, Val Accuracy: {result['val_accuracy'][-1]}, Val Loss: {result['val_loss'][-1]}")
+
+model.summary()
+
+model_json = model.to_json()
+with open("model_structure.json", "w") as json_file:
+    json_file.write(model_json)
+
+result_history = result.history
+print(result_history)
+
+
+
+import matplotlib.pyplot as plt
+fig = plt.figure(figsize = (20, 7))
+plt.subplot(121)
+plt.plot(result_history['accuracy'], label = 'acc')
+plt.plot(result_history['val_accuracy'], label = 'val_acc')
+plt.grid()
+plt.legend()
+
+plt.subplot(122)
+plt.plot(result_history['loss'], label = 'loss')
+plt.plot(result_history['val_loss'], label = 'val_loss')
+plt.grid()
+plt.legend()
+
